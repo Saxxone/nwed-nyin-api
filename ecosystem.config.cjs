d6a99a601ceb319a6b73fff23540e3847b1de4c5
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-12-27 20:50:21 +0000

    autodeploy

---
+++ ecosystem.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nwed-nyin-api',
+      script: 'npm run start',
+      port: 3000,
+      cwd: './',
+
+      env: {
+        NODE_ENV: 'production',
+      },
+
+      // Process management
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+
+      // Logging
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+
+      max_memory_restart: '1G', // Restart if memory usage exceeds 1GB
+    },
+  ],
+};
