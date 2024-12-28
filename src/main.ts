4c87d8d7149ad68f5a877b102d300dbbb54a5038
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-12-28 23:05:41 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'; // Import ValidationPipe
 
 async function bootstrap() {
-  const ui_base_url = process.env.UI_BASE_URL || 'https://www.nwednyin.org';
+  const ui_base_url = process.env.UI_BASE_URL || 'https://nwednyin.org';
   const api_base_url = process.env.API_BASE_URL || 'http://localhost:8080';
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [ui_base_url, 'https://www.nwednyin.org', 'https://nwednyin.org', /\.nwednyin\.org$/],
+    origin: ui_base_url,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
