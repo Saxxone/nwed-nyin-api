6277b59f385ed473f8fc8c75371d31c62c9496ee
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2025-01-18 20:50:42 +0000

    fix(cors): bug fix

--- src/dictionary/dictionary.module.ts
+++ src/dictionary/dictionary.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DictionaryService } from './dictionary.service';
 import { DictionaryController } from './dictionary.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from '../user/user.service';
 
 @Module({
   controllers: [DictionaryController],
-  providers: [DictionaryService, PrismaService],
+  providers: [DictionaryService, PrismaService, UserService],
 })
 export class DictionaryModule {}
