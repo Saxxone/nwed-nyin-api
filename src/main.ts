5b322a88c1f00c49a1b027dd6449aceefe22b582
Stephen Udoekpo <saxxone17@gmail.com>
2025-03-14 22:59:17 +0000

    replace pnom with npm

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as os from 'os';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const ui_base_url = process.env.UI_BASE_URL || 'https://www.nwednyin.org';
@@ -12,7 +13,18 @@ async function bootstrap() {
     preflightContinue: false,
   });
   app.useGlobalPipes(new ValidationPipe());
+
+  const network_interfaces = os.networkInterfaces();
+  const local_network_ip = Object.values(network_interfaces)
+    .flat()
+    .find((iface) => iface?.family === 'IPv4' && !iface.internal)?.address;
+
+  const accent = '\x1b[01m';
+  const reset = '\x1b[0m';
   await app.listen(8080);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(
+    `Application is running on: ${accent}${await app.getUrl()}${reset}`,
+    `\nLocal network on: ${accent}http://${local_network_ip}:${process.env.PORT ?? 8080}${reset}`,
+  );
 }
 bootstrap();
